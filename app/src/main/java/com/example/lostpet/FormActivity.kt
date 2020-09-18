b492b322132ca140d0cad77eef0c9d5ced929d69
Mathieu72000 <Math.corroy@gmail.com>
2020-09-18 21:25:09 +0200

    Add back arrow;

--- app/src/main/java/com/example/lostpet/FormActivity.kt
+++ app/src/main/java/com/example/lostpet/FormActivity.kt
@@ -1,8 +1,10 @@
 package com.example.lostpet
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.lostpet.fragment.FormFragment
+import kotlinx.android.synthetic.main.activity_form.*
 
 class FormActivity : AppCompatActivity() {
 
@@ -10,10 +12,26 @@ class FormActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
 
-        val formFragment = FormFragment.newInstance(intent.getBooleanExtra(Constants.IS_LOST, false), intent.getStringExtra(Constants.ANIMAL_ID))
+        setSupportActionBar(form_toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        val formFragment = FormFragment.newInstance(
+            intent.getBooleanExtra(Constants.IS_LOST, false),
+            intent.getStringExtra(Constants.ANIMAL_ID)
+        )
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.form_placeholder, formFragment)
             .commitAllowingStateLoss()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/example/lostpet/SettingsActivity.kt
+++ app/src/main/java/com/example/lostpet/SettingsActivity.kt
@@ -1,8 +1,10 @@
 package com.example.lostpet
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.lostpet.fragment.SettingsFragment
+import kotlinx.android.synthetic.main.activity_search.*
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -10,10 +12,23 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        setSupportActionBar(search_toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         val settingsFragment = SettingsFragment.newInstance()
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.settings_placeholder, settingsFragment)
             .commitAllowingStateLoss()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
